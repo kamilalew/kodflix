dab61df50c24719a9d30d357bd1e901e8d33414e
Kamila <kamila.m.lewicka@gmail.com>
2020-01-14 13:24:59 +0000

    Removed old imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import './App.css';
-// import eat_pray_love from './images/eat_pray_love.jpeg';  
-// import frida from './images/frida.jpg'; 
-// import grown_ups from './images/grown_ups.jpeg'; 
-// import perfect_strangers from './images/perfect_strangers.jpeg'; 
-// import twice_born from './images/twice_born.jpeg'; 
-// import zohan from './images/zohan.jpeg'; 
-// import Movies from './Movies.js';
 import Gallery from './Gallery.js';
 
 function App() {
