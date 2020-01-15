c020becf152bc27c0ab188f8f029747f0c2e5141
Kamila <kamila.m.lewicka@gmail.com>
2020-01-15 14:12:58 +0000

    Task #8 revised

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import Gallery from './Gallery.js';
+import Gallery from './components/Gallery';
+import Details from './components/Details'
 
 function App() {
   return (
-    <div className="App">
-      <Gallery />
-    </div>
+    <BrowserRouter>
+      <Route exact path='/' component={Gallery} />
+      <Route exact path='/details' component={Details} />
+    </BrowserRouter>
   );
 }
 
