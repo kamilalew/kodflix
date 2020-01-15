d3fe6a8f99a44f05c62982eb4d535aa31dea2598
Kamila <kamila.m.lewicka@gmail.com>
2020-01-15 20:58:06 +0000

    Task #10 unique details URL for each movie

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Gallery from './Gallery.js';
+import Details from './Details.js';
 
 function App() {
   return (
-    <div className="App">
-      <Gallery />
-    </div>
+    <BrowserRouter>
+      <Route exact path='/' component={Gallery} />
+      <Route exact path='/details' component={Details} />
+    </BrowserRouter>
   );
 }
 
