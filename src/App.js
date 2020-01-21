638bad7da06dc36a7a04d4b66e003e6621b300cc
Kamila <kamila.m.lewicka@gmail.com>
2020-01-21 13:11:11 +0000

    Added heading to all pages

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import './components/Gallery.css';
 import './App.css';
 import Gallery from './components/Gallery.js';
 import Details from './components/Details.js';
+import movie_icon from '../src/images/movie_icon.png';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Route exact path='/' component={Gallery} />
-      <Route exact path='/:movieId' component={Details} />
-    </BrowserRouter>
-  );
-}
-
-export default App;
+    <div>
+      <div class='background'>
+        <div className='main_title'>PEDRO ALMODOVAR</div>
+        <img src={movie_icon} className='movie_icon' alt='movie_icon'></img>
+      </div>
+        <BrowserRouter>
+          <Route exact path='/' component={Gallery} />
+          <Route exact path='/:movieId' component={Details} />
+        </BrowserRouter>
+      </div>
+      );
+    }
+    
+    export default App;
