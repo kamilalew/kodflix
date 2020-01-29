71893cc13d08eb189dc45b5bd180b033bc8a3262
Kamila <kamila.m.lewicka@gmail.com>
2020-01-29 13:45:00 +0000

    Serving frontend from backend - first part of task #23

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001
 const getGallery = require('./getGalleryBk.js')
+const path = require('path');
+
 
 app.get('/rest/movies/:id?', (req, res) => {
     res.send(getGallery(req.params.id))
 });
+
+
+app.use(express.static(path.join(__dirname, '../../build')));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
